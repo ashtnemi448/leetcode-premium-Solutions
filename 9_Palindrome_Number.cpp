e20cbdcee5faf5d68f7cc2cfb2db87d45a55c2e5
ashtnemi448 <atharwaadawadkar448@gmail.com>
2020-05-30 15:54:06 +0530

    .

--- 9_Palindrome_Number.cpp
+++
@@ -1,9 +0,0 @@
-class Solution 
-{
-public:
-    bool isPalindrome(int x) 
-    {
-        string x_str = to_string(x);
-        return equal(x_str.begin(), x_str.begin() + x_str.size()/2, x_str.rbegin());
-    }
-};
